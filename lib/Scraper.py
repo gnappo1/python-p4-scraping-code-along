bad327ef022d02207281d1821eb4cf851862e9ae
Matteo Piccini <gnappo.1@live.it>
2023-04-25 16:19:40 -0700

    done

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,28 @@ import ipdb
 
 
 class Scraper:
-    pass
+    def get_page(self):
+        headers = {'user-agent': 'my-app/0.0.1'}
+        html = requests.get("http://flatironschool.com/", headers=headers)
+        return BeautifulSoup(html.text, 'html.parser')
+    
+    def get_courses(self):
+        doc = self.get_page()
+        return doc.select(".row.bg-white.no-gutters.rounded-20.pad-10.panel-sticky")
+    
+    def make_courses(self):
+        courses = self.get_courses()
+        course_instances = []
+        for course in courses:
+            description = course.select("p")[0].text.strip()
+            title = course.select("h3")[0].text.strip()
+            schedule = "N/A"
+            course_instances.append(Course(title=title, schedule=schedule, description=description))
+        return course_instances
+    
+    def print_courses(self):
+        for course in self.make_courses():
+            print(course)
+
+scraper = Scraper()
+scraper.print_courses()
